Ported graph clustering calculation to CUDA. Also fixed some bugs with adjacency.

cugal/adjacency.py
@@ -56,11 +56,13 @@ class Adjacency:
         if not graph.is_directed():
             edges += [(y, x) for x, y in edges]
 
+        edge_count = len(edges)
+
         if has_cuda and "cuda" in str(device):
             edges = torch.tensor(sum(sorted(edges), ()),
                                  dtype=dtype, device=device)
             col_indices = torch.empty(
-                size=(len(edges),), dtype=dtype, device=device)
+                size=(edge_count,), dtype=dtype, device=device)
             row_pointers = torch.empty(
                 size=(node_count,), dtype=torch.int, device=device)
 
@@ -69,7 +71,9 @@ class Adjacency:
             return cls(col_indices, row_pointers)
 
         col_indices, row_pointers = \
-            torch.empty(size=(len(edges),)), torch.empty(size=(node_count,))
+            torch.empty(size=(edge_count,)), torch.empty(size=(node_count,))
+
+        row_pointers[0] = 0
 
         col_index_count, row_index = 0, 0
         for node_index, node_edges in groupby(sorted(edges), key=lambda edge: edge[0]):

cugal/feature_extraction.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 import cuda_kernels
 import torch
+from cugal.adjacency import Adjacency
 
 
 def graph_clustering(graph: nx.graph) -> np.ndarray:
@@ -9,15 +10,12 @@ def graph_clustering(graph: nx.graph) -> np.ndarray:
     return np.array([clustering[i] for i in graph.nodes()])
 
 
-def graph_clustering_cuda(graph: nx.graph) -> np.ndarray:
-    edges = list(nx.edges(graph))
-    if not graph.is_directed():
-        edges += [(y, x) for x, y in edges]
-    edges_tensor = torch.stack(list(map(torch.tensor, sorted(edges))))
-
-    clustering = torch.empty(nx.number_of_nodes(graph), dtype=torch.float)
-    cuda_kernels.graph_clustering(edges_tensor, clustering)
-
+def graph_clustering_cuda(graph: nx.graph) -> torch.Tensor:
+    adjacency = Adjacency.from_graph(graph, "cuda")
+    clustering = torch.zeros(nx.number_of_nodes(
+        graph), dtype=torch.float, device="cuda")
+    cuda_kernels.graph_clustering(
+        adjacency.col_indices, adjacency.row_pointers, clustering)
     return clustering
 
 

test/test_adjacency.py
@@ -6,6 +6,7 @@ from cugal.pred import dense_gradient, sparse_gradient
 import torch
 import random
 import networkx as nx
+import warnings
 
 
 def random_adjacency_matrix(size: int, device="cpu") -> torch.Tensor:
@@ -29,8 +30,11 @@ class TestAdjacency(unittest.TestCase):
         K = torch.randn(size=(size, size), dtype=torch.float32)
 
         grad = dense_gradient(A, B, P, K, 0)
-        sparse_grad = sparse_gradient(Adjacency.from_dense(A), Adjacency.from_dense(
-            B), Adjacency.from_dense(A.T), Adjacency.from_dense(B.T), P, K, 0)
+
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            sparse_grad = sparse_gradient(Adjacency.from_dense(A), Adjacency.from_dense(
+                B), Adjacency.from_dense(A.T), Adjacency.from_dense(B.T), P, K, 0)
 
         # This requres a bit high error tolerance.
         assert torch.allclose(grad, sparse_grad, rtol=1e-4, atol=1e-5)

test/test_feature_extraction.py
@@ -10,7 +10,7 @@ def random_graph(size: int) -> nx.graph:
 
 class TestAdjacency(unittest.TestCase):
     def test_graph_clustering_agree(self):
-        graph = random_graph(128)
+        graph = random_graph(32)
         correct = graph_clustering(graph)
-        cuda = graph_clustering_cuda(graph)
+        cuda = graph_clustering_cuda(graph).cpu().numpy()
         assert np.allclose(correct, cuda)