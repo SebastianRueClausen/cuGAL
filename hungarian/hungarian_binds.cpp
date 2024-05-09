9d6396a1867c944fe4ca945d6489a86ed034aa45
APersonH <andreashhhansen@gmail.com>
2024-05-09 10:06:36 +0000

    Added kernels.

---
+++ hungarian/hungarian_binds.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <torch/torch.h>
+#include <vector>
+
+std::vector<float> hungarian(const std::vector<std::vector<float>> &);
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    m.def("hungarian", &hungarian, "hungarian algorithm");
+    // m.def("main", &main, "test")
+}
---
+++ hungarian/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name='cuda_hungarian',
+    version='0.0.0',
+    ext_modules=[
+        CUDAExtension('cuda_hungarian', [
+            'hungarian_binds.cpp',
+            'hungarian.cu',
+        ],
+        extra_compile_args={
+            'nvcc': ["--threads", "20",
+                     "-gencode", "arch=compute_87,code=sm_87",
+                     "-gencode", "arch=compute_86,code=sm_86",
+            ]        }),
+    ],
+    cmdclass={
+        'build_ext': BuildExtension
+    })
