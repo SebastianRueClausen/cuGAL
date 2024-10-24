9a12df8a30daa9e0896fdcf51467638c1aba76d2
APersonH <Andreashhhansen@gmail.com>
2024-10-24 12:30:32 +0000

    improved analisys

--- benchmark/analyse.py
+++ benchmark/analyse.py
@@ -55,23 +55,33 @@ with open(json_file_path, 'r') as file:
 # Bar plot the accuracy with labels for each experiment
 cmap = plt.get_cmap('tab20')
 
+alg_desc = ExperimentResults.algorithms_descriptions(results.experiment.algorithms)
+
+def legend_without_duplicate_labels(labels):
+    unique = [l for i, l in enumerate(labels) if l not in labels[:i]]
+    plt.legend(unique, loc='center left', bbox_to_anchor=(1, 0.5))
+
 for i, res in enumerate(results.all_results()):
-    plt.bar(i, res[3].accuracy, label=f'{res[2]}', 
+    plt.bar(i, res[3].accuracy, label=alg_desc[res[2]],
             color=cmap(int.from_bytes(str(res[2]).encode(), 'little') % 20))
 
-#plt.legend()
+legend_without_duplicate_labels(plt.gca().get_legend_handles_labels()[1])
+
 plt.xlabel('Experiment')
 plt.ylabel('Accuracy')
 plt.title('Accuracy of Experiments')
-plt.savefig('accuracy.png')
+plt.savefig('accuracy.png', dpi=300, bbox_inches = "tight")
 
 # Bar plot the time with labels for each experiment
 plt.clf()
 for i, res in enumerate(results.all_results()):
-    plt.bar(i, res[3].profile.time, label=f'{res[2]}', 
+    plt.bar(i, res[3].profile.time, label=alg_desc[res[2]], 
             color=cmap(int.from_bytes(str(res[2]).encode(), 'little') % 20))
     
+legend_without_duplicate_labels(plt.gca().get_legend_handles_labels()[1])
+
+plt.legend()
 plt.xlabel('Experiment')
 plt.ylabel('Time (s)')
 plt.title('Time of Experiments')
-plt.savefig('time.png')
+plt.savefig('time.png', dpi=300, bbox_inches = "tight")
