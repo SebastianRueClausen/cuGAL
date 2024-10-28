fixed bug with averageing multiple graphs

benchmark/analyse.py
@@ -1,7 +1,9 @@
 import json
+import re
 import matplotlib.pyplot as plt
 import readline
 import os
+import numpy as np
 from experiment import ExperimentResults
 
 def complete_path(text, state):
@@ -49,8 +51,14 @@ while True:
         print('File not found, try again.')
 
 # Load JSON data
-with open(json_file_path, 'r') as file:
-    results = ExperimentResults.from_dict(json.load(file))
+file = open(json_file_path, 'r')
+results = ExperimentResults.from_dict(json.load(file))
+
+num_noises  = len(results.experiment.noise_levels)
+num_results = len(list(results.all_results()))
+num_graphs  = len(results.experiment.graphs)
+num_algs    = len(results.experiment.algorithms)
+print("num_noises ", num_noises, " num_results ", num_results, " num_graphs ", num_graphs, " num_algs ", num_algs)
 
 # Bar plot the accuracy with labels for each experiment
 cmap = plt.get_cmap('tab20')
@@ -61,27 +69,39 @@ def legend_without_duplicate_labels(labels):
     unique = [l for i, l in enumerate(labels) if l not in labels[:i]]
     plt.legend(unique, loc='center left', bbox_to_anchor=(1, 0.5))
 
-for i, res in enumerate(results.all_results()):
-    plt.bar(i, res[3].accuracy, label=alg_desc[res[2]],
-            color=cmap(int.from_bytes(str(res[2]).encode(), 'little') % 20))
+def draw_x_axis_labels():
+    x_pos = np.linspace(num_results / num_noises / 2, num_results - (num_results / num_noises / 2), num_noises)
+    plt.xticks(x_pos, [str(n.source_noise*100) + "% Noise" for n in results.experiment.noise_levels])
 
+    ## Draw vertical lines (I am not including the lines at the ends)
+    pos = [((i)*num_algs-0.5) for i in range(num_graphs+1)]
+#    plt.vlines(pos[1:-1], 0, -0.4, color='black', lw=1.5, clip_on=False, transform=plt.gca().get_xaxis_transform())
+
+    ## Draw second level axes ticklables
+    for ps0, ps1, lbl in zip(pos[:-1], pos[1:], [re.sub(" {|, ", "\n{", str(g)) for g in results.experiment.graphs]):
+        plt.text((ps0 + ps1) / 2, -0.16, lbl, ha='center', clip_on=False, transform=plt.gca().get_xaxis_transform(), weight = 'bold', size=6)
+    for i, res in enumerate(results.all_results()):
+        plt.bar(i, res[3].accuracy, label=alg_desc[res[2]],
+                color=cmap(int.from_bytes(str(res[2]).encode(), 'little') % 20))
+
+## Draw the Accuracy plot
+draw_x_axis_labels()
 legend_without_duplicate_labels(plt.gca().get_legend_handles_labels()[1])
 
-plt.xlabel('Experiment')
+
 plt.ylabel('Accuracy')
 plt.title('Accuracy of Experiments')
-plt.savefig('accuracy.png', dpi=300, bbox_inches = "tight")
+plt.savefig(file.name + '_accuracy.png', dpi=300, bbox_inches = "tight")
 
-# Bar plot the time with labels for each experiment
+# Draw the Time plot
 plt.clf()
 for i, res in enumerate(results.all_results()):
     plt.bar(i, res[3].profile.time, label=alg_desc[res[2]], 
             color=cmap(int.from_bytes(str(res[2]).encode(), 'little') % 20))
     
+draw_x_axis_labels()
 legend_without_duplicate_labels(plt.gca().get_legend_handles_labels()[1])
 
-plt.legend()
-plt.xlabel('Experiment')
 plt.ylabel('Time (s)')
 plt.title('Time of Experiments')
-plt.savefig('time.png', dpi=300, bbox_inches = "tight")
+plt.savefig(file.name + '_time.png', dpi=300, bbox_inches = "tight")

benchmark/experiment.py
@@ -364,8 +364,13 @@ class Experiment:
             
             for noise_level in self.noise_levels:
                 noise_results = []
-                source, target, source_mapping, _ = generate_graph(
-                    source_graph, target_graph, generator, noise_level)
+                sources, targets, source_mappings, target_mappings = [], [], [], []
+                for i in range(self.num_runs):
+                    source, target, source_mapping, _ = generate_graph(
+                        source_graph, target_graph, generator, noise_level)
+                    sources.append(source)
+                    targets.append(target)
+                    source_mappings.append(source_mapping)
                 
                 # save svg of graph
                 # nx.draw(source, with_labels=False, node_size=2)
@@ -374,15 +379,15 @@ class Experiment:
                 for algorithm in self.algorithms:
                     # Run multiple times to get average results
                     run_results = []
-                    for _ in range(self.num_runs):
+                    for i in range(self.num_runs):
                         profile = Profile()
                         if algorithm.use_fugal:
                             start_time = TimeStamp('cpu')
                             _, answer = Fugal.main(
                                 {"Src": edges_to_adjacency_matrix(np.array(source.edges),
-                                                                source_mapping.shape[0]),
+                                                                source_mappings[i].shape[0]),
                                 "Tar": edges_to_adjacency_matrix(np.array(target.edges),
-                                                                source_mapping.shape[0])},
+                                                                source_mappings[i].shape[0])},
                                 algorithm.config.iter_count,
                                 True, algorithm.config.mu
                             )
@@ -390,7 +395,7 @@ class Experiment:
                                 'cpu').elapsed_seconds(start_time)
                         else:
                             _, answer = cugal(
-                                source, target, algorithm.config, profile)
+                                sources[i], targets[i], algorithm.config, profile)
                         
                         if self.save_alignment:
                             with open(str(datetime.datetime.now()) + ".txt", "w") as f:
@@ -399,10 +404,10 @@ class Experiment:
                         
                         run_results.append(Result.calculate(
                             profile,
-                            nx.to_numpy_array(source),
-                            nx.to_numpy_array(target),
+                            nx.to_numpy_array(sources[i]),
+                            nx.to_numpy_array(targets[i]),
                             np.array([x for _, x in answer]),
-                            source_mapping,
+                            source_mappings[i],
                         ))
 
                     noise_results.append(Result.average(run_results))